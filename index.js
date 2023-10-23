875d7a2153bfa34deeade05da32c9a13bc3bfbc2
Ye Htet <yhtet1934@gmail.com>
2023-10-23 21:05:02 +0630

    revised function parameter lab

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function introduction(name) {
+    return `Hi my name is ${name}.`
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}`;
+}
+
 function introducitonWithLanguageOptional(name, language = "Javascript") {
-    console.log(name, language);
+    return `Hi, my name is ${name} and I am learning to program in ${language}`;
 }
